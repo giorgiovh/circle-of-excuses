ca66b9de692aa3c475fe3194ccaacdc98a401d29
giorgiovh <jorgevonhoroch@gmail.com>
2022-11-09 17:01:42 -0500

    adds styling on App.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
+    <div className='home'>
       <h2>Home</h2>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, officiis numquam hic odio deleniti, quo tenetur fugiat maiores ducimus labore consequuntur dolor aliquam in vero explicabo beatae. Incidunt, quidem quis!</p>
     </div>
