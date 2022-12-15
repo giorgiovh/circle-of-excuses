608bfbbb49207b99f6a7a07c6f5093a0e5a44e6f
giorgiovh <jorgevonhoroch@gmail.com>
2022-12-15 18:48:28 -0500

    adds margin above and below the spin button

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,8 @@ import Button from '@mui/material/Button';
 
 import Excuse from '../../components/Excuse'
 
+import './Home.css';
+
 export default function Home() {
   const { data: excuses, isPending, error } = useFetch('http://localhost:3000/excuses')
   const [excuse, setExcuse] = useState(null)
@@ -15,7 +17,13 @@ export default function Home() {
 
   return (
     <div className='home'>
-      <Button variant="contained" onClick={() => generateRandomExcuseId()}>Spin the wheel!</Button>
+      <Button 
+        variant="contained" 
+        onClick={() => generateRandomExcuseId()}
+        className='spin-button'
+      >
+        Spin the wheel!
+      </Button>
       {isPending && <div>Loading...</div>}
       {error && <div>{error}</div>}
       {excuse && <Excuse name={excuse.name} description={excuse.description} response={excuse.response} socraticResponse={excuse.socraticResponse}/>}
