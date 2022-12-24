2ad02401d65690581394e49f80a86660bd653244
giorgiovh <jorgevonhoroch@gmail.com>
2022-12-24 15:16:57 -0300

    adds firebase and fetches data on home component from firestore

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAjEduEa0XXMnpCGcbEvts3zquRJkN3j9s",
+  authDomain: "circle-of-excuses-site.firebaseapp.com",
+  projectId: "circle-of-excuses-site",
+  storageBucket: "circle-of-excuses-site.appspot.com",
+  messagingSenderId: "584891291324",
+  appId: "1:584891291324:web:81a3dea3a4d35b70b2a2b9"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,38 @@
-import { useFetch } from '../../hooks/useFetch';
+import { useEffect, useState } from 'react';
+import { projectFirestore } from '../../firebase/config'
 import ExcuseList from '../../components/ExcuseList';
 
 import './Home.css';
 
 export default function Home() {
-  const { data: excuses, isPending, error } = useFetch('http://localhost:3000/excuses')
+  const [excuses, setExcuses] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    const unsub = projectFirestore.collection('excuses').onSnapshot((snapshot) => {
+      if (snapshot.empty) {
+        setError('No excuses to load')
+        setIsPending(false)
+      } else {
+        let results = []
+        snapshot.docs.forEach(doc => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        setExcuses(results)
+        setIsPending(false)
+      }
+    }, (err) => {
+      setError(err.message)
+      setIsPending(false)
+    })
+
+    // This "unsub" clean-up function allows us to stop listening to the changes (ie. it unsubscribes from the onSnapshot listener) once the component unmounts. It gets called automatically once the component unmounts
+    return () => unsub()
+  }, [])
+
 
   return (
     <div className='home'>
