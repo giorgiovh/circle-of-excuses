d3831ea5dc276e0b6d69053a36b2f8ec0833a726
giorgiovh <jorgevonhoroch@gmail.com>
2023-01-01 18:25:34 -0300

    adds cleanup functions for useLogout and useSignup hooks

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { projectAuth } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
-
 
 export const useLogout = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const [isCancelled, setIsCancelled] = useState(false)
   const { dispatch } = useAuthContext()
 
   const logout = async () => {
@@ -16,17 +16,28 @@ export const useLogout = () => {
     try {
       await projectAuth.signOut()
 
-      // dispatch logout action
+      // dispatch logout action to update global state
       dispatch({ type:'LOGOUT' })
 
-      setIsPending(false)
-      setError(null)
+      // update local state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
     } catch (err) {
-      console.log(err.message);
-      setError(err.message)
-      setIsPending(false)
+      // update local state
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
+
+  useEffect(() => {
+    // since this function is being returned inside useEffect, it will run only if the component is unmounted
+    return () => setIsCancelled(true)
+  }, [])
 
   return { error, isPending, logout}
 }
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,10 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { projectAuth } from '../firebase/config'
 import { useAuthContext } from "./useAuthContext"
 
 export const useSignup = () => {
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
+  const [isCancelled, setIsCancelled] = useState(false)
   const { dispatch } = useAuthContext()
 
   const signup = async (email, password, displayName) => {
@@ -22,17 +23,28 @@ export const useSignup = () => {
       // add display name to user
       await res.user.updateProfile({ displayName })
       
-      // dispatch login action
+      // dispatch login action to update global state
       dispatch({ type: 'LOGIN', payload: res.user })
 
-      setIsPending(false)
-      setError(null)
+      // update local state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
     } catch (err) {
-      console.log(err.message)
-      setError(err.message)
-      setIsPending(false)
+      // update local state
+      if (!isCancelled) {
+        console.log(err.message);
+        setError(err.message)
+        setIsPending(false)
+      }
     }
   }
+
+  useEffect(() => {
+    // since this function is being returned inside useEffect, it will run only if the component is unmounted
+    return () => setIsCancelled(true)
+  }, [])
 
   return { error, isPending, signup }
 }
