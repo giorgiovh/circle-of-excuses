7f25d1d3b35211cd7c244693d566bcdba90df1fd
giorgiovh <jorgevonhoroch@gmail.com>
2023-01-15 18:34:22 -0500

    adds wrapper component to conditionally use the useCollection('excuses') hook so that it only gets called when there is a user logged in

---
+++ src/hooks/WrapperForUseCollectionUser.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react"
+import { useCollection } from "./useCollection"
+
+export default function WrapperForUseCollectionUser({ uid, setIsUserPending, setUserError, setUserExcuses}) {
+  const { isPending, error, documents } = useCollection(
+    'excuses',
+    ["uid", "==", uid],
+    ["createdAt", "desc"]
+  )
+
+  useEffect(() => {
+    setIsUserPending(isPending)
+    setUserError(error)
+    setUserExcuses(documents)
+  }, [setIsUserPending, setUserError, setUserExcuses, isPending, error, documents])
+  
+  return null
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,26 @@
 import { useState, useEffect } from 'react';
 import { useCollection } from '../../hooks/useCollection';
 import ExcuseList from '../../components/ExcuseList';
+import WrapperForUseCollectionUser from '../../hooks/WrapperForUseCollectionUser';
 
 import './Home.css';
 
 export default function Home({ uid }) {
   const [presetExcuses, setPresetExcuses] = useState([]);
   const [userExcuses, setUserExcuses] = useState([]);
+  const [userError, setUserError] = useState(null);
+  const [isUserPending, setIsUserPending] = useState(false);
 
   // fetch preset excuses
   const { isPending: isPresetPending, error: presetError, documents: presetDocuments } = useCollection("preset_excuses");
 
   // fetch user excuses
   // TODO: Figure out how to only call useCollection('excuses') if a user is logged in (ie. if there is a uid). Otherwise, I get an error saying "Missing or insufficient permissions."
-  const { isPending: isUserPending, error: userError, documents: userDocuments } = useCollection(
-    'excuses',
-    uid ? ["uid", "==", uid] : null,
-    ["createdAt", "desc"]
-  )
+  // const { isPending: isUserPending, error: userError, documents: userDocuments } = useCollection(
+  //   'excuses',
+  //   uid ? ["uid", "==", uid] : null,
+  //   ["createdAt", "desc"]
+  // )
 
   useEffect(() => {
     if (!presetError && presetDocuments) {
@@ -25,11 +28,11 @@ export default function Home({ uid }) {
     }
   }, [presetDocuments, presetError]);
 
-  useEffect(() => {
-    if (!userError && userDocuments) {
-        setUserExcuses(userDocuments);
-    }
-  }, [userDocuments, userError]);
+  // useEffect(() => {
+  //   if (!userError && userDocuments) {
+  //       setUserExcuses(userDocuments);
+  //   }
+  // }, [userDocuments, userError]);
   
   let excusesToDisplay = []
   if (!uid && presetExcuses) {
@@ -38,11 +41,15 @@ export default function Home({ uid }) {
     excusesToDisplay = [...presetExcuses, ...userExcuses]
   }
 
+  console.log('isPresetPending', isPresetPending);
+  console.log('isUserPending', isUserPending);
   return (
     <div className='home'>
       {(isPresetPending || isUserPending) && <div>Loading...</div>}
       {userError && <div>{userError}</div>}
       {!uid && <p>Sign up or log in to create your own excuses and build your personal collection</p>}
+      {/* The below wrapper component is used to conditionally use the useCollection('excuses') hook only when there is a user logged in. React doesn't allow using a hook conditionally so this is the workaround*/}
+      {uid && <WrapperForUseCollectionUser uid={uid} setUserExcuses={setUserExcuses} setUserError={setUserError} setIsUserPending={setIsUserPending}/>}
       {excusesToDisplay.length > 0 && <ExcuseList uid={uid} excuses={excusesToDisplay} />}
     </div>
   )
