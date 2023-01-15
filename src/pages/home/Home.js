adeb2582c7cdfbdfe87aaa1161b8b986e4e87fa7
giorgiovh <jorgevonhoroch@gmail.com>
2023-01-15 17:08:11 -0500

    fetches data from public and user created collections

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,24 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useCollection } from '../../hooks/useCollection';
 import ExcuseList from '../../components/ExcuseList';
 
 import './Home.css';
 
 export default function Home({ uid }) {
+  const [publicExcuses, setPublicExcuses] = useState([]);
+  const [userExcuses, setUserExcuses] = useState([]);
 
-  const { isPending, error, documents } = useCollection(
+  const { isPending: isPublicPending, error: publicError, documents: publicDocuments } = useCollection("preset_excuses");
+  const { isPending: isUserPending, error: userError, documents: userDocuments } = useCollection(
     'excuses',
     // if a user is logged in, query the pre-set excuses and the user-created excuses. Else, just query the pre-set excuses
     uid ? ["uid", "==", uid] : null,
     ["createdAt", "desc"]
   )
+
+  useEffect(() => {
+    if (!publicError && !userError && publicDocuments && userDocuments) {
+        setPublicExcuses(publicDocuments);
+        setUserExcuses(userDocuments);
+    }
+  }, [publicDocuments, userDocuments, publicError, userError]);
   
+  let allExcuses = []
+  if (!uid && publicExcuses) {
+    allExcuses = publicExcuses
+  } else if (uid && publicExcuses && userExcuses) {
+    allExcuses = [...publicExcuses, ...userExcuses]
+  }
+  // allExcuses = publicExcuses && userExcuses ? [...publicExcuses, ...userExcuses] : [];
+
   return (
     <div className='home'>
-      {isPending && <div>Loading...</div>}
-      {error && <div>{error}</div>}
+      {(isPublicPending || isUserPending) && <div>Loading...</div>}
+      {userError && <div>{userError}</div>}
       {!uid && <p>Sign up or log in to create your own excuses and build your personal collection</p>}
-      {documents && <ExcuseList uid={uid} excuses={documents} />}
+      {allExcuses.length > 0 && <ExcuseList uid={uid} excuses={allExcuses} />}
     </div>
   )
 }
