926ac3ba45fbf63b6447df71a77e7f3c64f3c3f1
giorgiovh <jorgevonhoroch@gmail.com>
2023-01-16 13:20:56 -0500

    fixes issue where when clicking on SEE EXCUSE for a document in the search results, it wasn't opening that excuse

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -12,36 +12,36 @@ export default function Search({ uid }) {
   const queryParams = new URLSearchParams(queryString);
   const query = queryParams.get('q')
 
-  // search for excuses with the name "query" in the "preset_excuses" collection first and if "uid" is not null, also search in the "excuses" collection. Then combine the results from both searches and set the documents state equal to the results from both searches.
+  // search for excuses with the name "query" in the "preset_excuses" collection first and if the user is logged in (ie. there is a "uid" value), also search in the "excuses" collection. Then combine the results from both searches and set the documents state equal to the results from both searches.
 
   useEffect(() => {
     let presetExcuses = [];
     let excuses = [];
-      projectFirestore.collection("preset_excuses").where("name", "==", query).get()
+    projectFirestore.collection("preset_excuses").where("name", "==", query).get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          presetExcuses.push({...doc.data(), id: doc.id});
+        });
+        setDocuments([...presetExcuses])
+      })
+      .catch((error) => {
+        console.log('Could not fetch the data');
+        setError(error.message)
+      });
+    if (uid) {
+      projectFirestore.collection("excuses").where("name", "==", query).where("uid", "==", uid).get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            presetExcuses.push(doc.data());
+            excuses.push({...doc.data(), id: doc.id});
           });
-          setDocuments([...presetExcuses])
+          setDocuments([...presetExcuses, ...excuses])
         })
         .catch((error) => {
-          console.log("Error getting documents: ", error);
+          console.log('Could not fetch the data');
+          setError(error.message)
         });
-      if (uid) {
-        projectFirestore.collection("excuses").where("name", "==", query).where("uid", "==", uid).get()
-          .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-              excuses.push(doc.data());
-            });
-            setDocuments([...presetExcuses, ...excuses])
-          })
-          .catch((error) => {
-            console.log("Error getting documents: ", error);
-          });
-      }
+    }
   }, [query, uid])
-
-  console.log(documents);
 
   return (
     <div>
