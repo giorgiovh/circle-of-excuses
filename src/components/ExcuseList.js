c6df2f6921810156329c93c5abf9a4809bfaea44
giorgiovh <jorgevonhoroch@gmail.com>
2023-03-24 14:29:26 -0400

    stack cards when page shrinks

--- src/components/ExcuseList.js
+++ src/components/ExcuseList.js
@@ -1,18 +1,39 @@
-import ImgMediaCard from './ImgMediaCard';
+// import ImgMediaCard from './ImgMediaCard';
 
-import './ExcuseList.css'
+// import './ExcuseList.css'
+
+// export default function ExcuseList({ uid, excuses }) {
+
+//   if (excuses.length === 0) {
+//     return <div>No excuses to load...</div>
+//   }
+
+//   return (
+//     <div className='card-grid'>
+//       {excuses.map(excuse => (
+//         <ImgMediaCard key={excuse.id} excuse={excuse} />
+//       ))}
+//     </div>
+//   )
+// }
+
+import ImgMediaCard from './ImgMediaCard';
+import { Grid } from '@mui/material';
+import './ExcuseList.css';
 
 export default function ExcuseList({ uid, excuses }) {
-
   if (excuses.length === 0) {
-    return <div>No excuses to load...</div>
+    return <div>No excuses to load...</div>;
   }
 
   return (
-    <div className='card-grid'>
+    <Grid container spacing={2} justifyContent="flex-start">
       {excuses.map(excuse => (
-        <ImgMediaCard key={excuse.id} excuse={excuse} />
+        <Grid item xs={12} sm={6} md={4} key={excuse.id}>
+          <ImgMediaCard excuse={excuse} />
+        </Grid>
       ))}
-    </div>
-  )
+    </Grid>
+  );
 }
+
